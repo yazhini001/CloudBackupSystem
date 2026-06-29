bbc14f2fca8012a6a4a6f6f7699529cd49cdbe9c
yazhini <yazhiniyazh2006@gmail.com>
2026-06-29 15:40:22 +0530

    Configure Railway MySQL and prepare for deployment

--- app.py
+++ app.py
@@ -17,10 +17,20 @@ cloudinary.config(
 
 db = pymysql.connect(
     host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT")),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
     database=os.getenv("DB_NAME")
 )
+def get_db():
+    return pymysql.connect(
+        host=os.getenv("DB_HOST"),  
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        autocommit=True
+    )
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
