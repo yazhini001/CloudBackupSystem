4be9a3c7d35841cb5a217c353041e1011290b00e
yazhini <yazhiniyazh2006@gmail.com>
2026-06-29 16:45:01 +0530

    update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ db = pymysql.connect(
 )
 def get_db():
     return pymysql.connect(
-        host=os.getenv("DB_HOST"),  
+        host=os.getenv("DB_HOST"),
         port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
